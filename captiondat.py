09f7076566f97e081bf140131193e52a29f5a1ca
Dhruv Verma <t0916994@kelaberetiv.sg>
2019-03-19 16:41:09 +0800

    first commit

---
+++ captiondat.py
@@ -0,0 +1,99 @@
+#%%
+import os
+import io
+from contextlib import redirect_stdout
+from pickle import load
+from numpy import argmax
+from IPython.display import Image, display
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+from keras.models import load_model
+from gpt2_Pytorch.main import gpt2
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+# extract features from the photo
+def extract_features(filename):
+    # load the model
+    model = VGG16()
+    # re-structure the model
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    # load the photo
+    image = load_img(filename, target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # get features
+    feature = model.predict(image, verbose=0)
+    return feature
+ 
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+ 
+# generate a description for an image
+def generate_desc(model, tokenizer, photo, max_length):
+    # seed the generation process
+    in_text = 'startseq'
+    # iterate over the whole length of the sequence
+    for _ in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = model.predict([photo,sequence], verbose=0)
+        # convert probability to integer
+        yhat = argmax(yhat)
+        # map integer to word
+        word = word_for_id(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)        
+    return final
+
+
+#%%
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+# pre-define the max sequence length (from training)
+max_length = 34
+# load the model
+model = load_model('run9_20e_6b/model-11-3.7910.hdf5')
+# load and prepare the photograph
+filename = 'example12.jpg'
+display(Image(filename=filename))
+photo = extract_features(filename)
+# generate description
+description = generate_desc(model, tokenizer, photo, max_length)
+print(description)
+# print GPT2 output using description
+f = io.StringIO()
+with redirect_stdout(f):
+    gpt2(description,quiet=True)
+out = f.getvalue()
+print(out)
+
+
+#%%
+description = 'playing basketball these two little boys pulled the ball and scored a three pointer fo'
+gpt2(description,quiet=True)
